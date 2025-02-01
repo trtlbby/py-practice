fa19e00a29f0db62369f7dabe940e6022b5f87bf
trtlbby <earllawrence.bacsain@unc.edu.ph>
2025-02-02 01:12:04 +0800

    🛠️Learned assigning variables and printing

---
+++ py-tutorial.py
@@ -0,0 +1,14 @@
+#A tutorial to familiarize with Python!!!
+
+#Getting started with variables.
+#I can start by doing this: Let's use x and y for main variable name globally
+
+#Start:
+x = 0
+y = 1
+print(x, y)
+#Output: 0 1
+#Compare to Java that im familiar with Python doesn't use + sign when printing two value
+
+#Hello world
+print("Hello, World!")
