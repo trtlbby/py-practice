6f2e7e5096cd11d090ca17e214d86afd8c127b3b
trtlbby <earllawrence.bacsain@unc.edu.ph>
2025-02-04 21:50:58 +0800

    🛠️To fix the collection [KeyError]

--- py-tutorial.py
+++ py-tutorial.py
@@ -1,4 +1,5 @@
 #A tutorial to familiarize with Python!!!
+from operator import ifloordiv
 
 #Getting started with variables.
 #I can start by doing this: Let's use x and y for main variable name globally
@@ -19,3 +20,17 @@ print("Hello, World!") #merge this to v0.2
 # using for loop
 for w in fruits:
     print(w, len(w))
+
+
+#Create a collection
+user_ID = {'Sab': '1234', 'Jonas': '4325', 'Linly': '143'}
+
+#Iterate over a copy?
+for user, id_num in user_ID.copy().items():
+    if id_num == '1234':
+        del user_ID[id_num]
+
+remaining_user = {}
+for user, id_num in user_ID.copy().items():
+    if id_num == '143':
+        remaining_user[user_ID] = id_num
